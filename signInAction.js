18639bbe98b60b1e3fd5e5d9c3c4a16b8b44c6d5
Amr AbdulSalam <amrsalam@hotmail.com>
2021-11-13 10:49:29 +0200

    add js file

---
+++ signInAction.js
@@ -0,0 +1,11 @@
+var usernameText, passwrodText
+
+
+function submitButton() {
+    //when submit is clicked 
+    usernameText = document.getElementById('usernameTextId').value
+    passwrodText = document.getElementById('passwordTextId').value
+    console.log(passwrodText)
+    //console.log("object")
+
+}
